074bf375ef1e302803313703577e59b7309dac6d
kinson <swilkinsonhunter@gmail.com>
2021-06-28 18:31:28 -0600

    use purgecss to remove unused tailwind styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
-const tailwindcss = require('tailwindcss');
+const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
-    ],
+  plugins: [
+    tailwindcss("./tailwind.js"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production" ? require("autoprefixer") : null,
+    process.env.NODE_ENV === "production" ? cssnano({ preset: "default" }) : null,
+    purgecss({
+      content: ["./index.html"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+  ],
 };
